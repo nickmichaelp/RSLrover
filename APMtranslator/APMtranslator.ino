a0f543a8233f89779f05d4b43ec54712bf59a127
nickmichaelp <NickPeacock@Nicholass-MacBook-Pro.local>
2015-01-29 17:26:41 -0800

    the merge didn't work

--- APMtranslator/APMtranslator.ino
+++ APMtranslator/APMtranslator.ino
@@ -1,15 +1,17 @@
-<<<<<<< HEAD
-
-=======
 int Serial_timeout = 25; //Serial Timout
 
+/*serial strings*/
 String commandType = "C";
 String commandMode;
 String throttleORsteer;
+String toSend;
 
 //input Pin initializations
 int steering_pot_pin = 0;
 int speed_pot_pin = 1;
+
+//flags
+bool speedFlag = true;
 
 
 void setup(){
@@ -17,34 +19,43 @@ void setup(){
   Serial.setTimeout(Serial_timeout);
 }
 
-int APMconverter(analogSpeed){
+int mapAPM(int analog){
   /*
-  *this converts the 0-5V output of the APM to eqivalent serial string commands
+  *this converts the 0-5V output of the APM to eqivalent serial string commands, [-1000,1000]
   */
-  
-  return map(analogSpeed, 0, 5, -1000, 1000);
+  return map(analog, 0, 5, -1000, 1000);
 }
 
 int rc_apm_function(){
   /*
   *This function turns the APM output into the serial sgrings the Rover understands
   */
-  bool speedFlag = true;
-  int analogSpeed;
-  int desiredSpeed;
+  int analogSpeed, analogSteering;
+  int desiredSpeed, desiredSteering;
   if(true)//this will be act_gear == desiredGear
   {
+    commandMode = "A"; //We think we need it in acatuator but are not sure
     if(speedFlag)
     {
-      command_mode = "A"; //We think we need it in acatuator but are not sure
       analogSpeed = analogRead(speed_pot_pin);
       desiredSpeed = mapAPM(analogSpeed);
+      throttleORsteer = "V";
+      toSend = commandType + "," + commandMode + "," + throttleORsteer + "," + desiredSpeed;
+      Serial.println(toSend);
     }
+    else{
+      analogSteering = analogRead(steering_pot_pin);
+      desiredSteering = mapAPM(analogSteering);
+      throttleORsteer = "W";
+      toSend = commandType + "," + commandMode + "," + throttleORsteer + "," + desiredSpeed;
+      Serial.println(toSend);
+    }
+    speedFlag = !speedFlag;
   }
+  else{}//if the desired gear does not match actual
   
 }
   
 void loop(){
-  
+  rc_apm_function();
 }
->>>>>>> Nick
