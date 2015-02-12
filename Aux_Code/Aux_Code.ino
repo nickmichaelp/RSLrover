d8bc7a75a4940d0df6933e3460f3b3d3055552d2
RSLrover <npeacock@scu.edu>
2015-02-12 14:50:21 -0800

    aux Code

---
+++ Aux_Code/Aux_Code.ino
@@ -0,0 +1,26 @@
+int auxA = 22;
+
+String stringFromConsole;
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial2.begin(9600);
+  pinMode(auxA, OUTPUT);
+}
+
+void loop() {
+  if(Serial1.available()){
+    stringFromConsole = Serial.readStringUntil('\r');
+    if(stringFromConsole[0] == 'A'){
+      if(stringFromConsole[1] == 'A'){
+        digitalWrite(auxA, HIGH);
+      }
+      else{
+        digitalWrite(auxA, LOW);
+      }
+    }
+    Serial2.println(stringFromConsole);
+  }
+
+}
